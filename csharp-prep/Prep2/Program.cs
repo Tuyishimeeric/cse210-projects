998a732a4d2fa06a0648a54267709d2fd655dffb
Tuyishimeeric <51074730+Tuyishimeeric@users.noreply.github.com>
2024-05-01 21:17:47 +0200

    prep2 updated
    creating a grading program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("what is your grade percantage?");
+       string userInput = Console.ReadLine();
+       int grade = int.Parse(userInput);
+
+       string letter = "";
+
+       if (grade >=90)
+       {
+       //Console.WriteLine("the grade is A");
+        letter = "A";
+
+       }
+
+       else if (grade >= 80)
+       {
+       //Console.WriteLine("the grade is B");
+        letter = "B";
+       }
+
+       else if (grade >= 70)
+       {
+       //Console.WriteLine("the grade is C");
+        letter = "C";
+       }
+
+       else if (grade >= 60)
+       {
+       //Console.WriteLine("the grade is D");
+        letter ="D";
+       }
+
+       else
+       {
+
+       //Console.WriteLine("The grade is F");
+       letter ="F";
+       }
+
+       Console.WriteLine($"the grade is: {letter}");
+
+       if (grade >= 70)
+       {
+        Console.WriteLine("You have passed!");
+       }
+      else
+      {
+        Console.WriteLine("Try again");
+      }
+
+    
+
     }
 }
