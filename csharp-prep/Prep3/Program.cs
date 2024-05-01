dca81b1637f110659b687e9b994fa8aa4905c5a0
Tuyishimeeric <51074730+Tuyishimeeric@users.noreply.github.com>
2024-05-01 22:04:56 +0200

    prep3 update
    guessing the magic number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+       //Console.Write("guess the number");
+        //string userInput = Console.ReadLine();
+        //int number = int.Parse(userInput);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 25);
+
+
+        int guess = 4;
+
+        while (guess != number)
+        {
+        Console.Write("what is your guess?");
+        guess = int.Parse(Console.ReadLine());
+
+        if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        
+
+
+
+
+
     }
 }
